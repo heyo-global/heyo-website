ab63c1699b560e646bc0e48965187767010a9ad2
chengmingyan <345021109@qq.com>
2024-11-12 20:24:31 +0800

    fix copy

--- components/home/PageTwo.tsx
+++ components/home/PageTwo.tsx
@@ -34,19 +34,19 @@ const PageTwo = () => {
       <HeaderTwo />
       <img
         src="/bg-light.svg"
-        className="w-full h-screen absolute bottom-0 left-0 z-[1] img-disabled"
+        className=" w-full h-screen absolute bottom-0 left-0 z-[6] img-disabled"
         alt=""
       />
 
       <img
         src="/image-footer.png"
-        className="w-full h-[19.3vw] hidden md:block  absolute bottom-0 left-0 z-[2] img-disabled"
+        className="w-full h-[19.3vw] hidden md:block  absolute bottom-0 left-0 z-[10] img-disabled"
         alt=""
       />
 
       <img
         src="/image-footer-phone.png"
-        className="w-full h-[32.27vw] md:hidden block absolute bottom-0 left-0 z-[2] img-disabled"
+        className="w-full h-[32.27vw] md:hidden block absolute bottom-0 left-0 z-[10] img-disabled"
         alt=""
       />
 
@@ -54,7 +54,7 @@ const PageTwo = () => {
       
       <div
         ref={ref}
-        className="md:min-w-[29.2vw] md:h-[18.3vw] px-[20px] min-w-[60.3vw] h-[35.7vw] -mt-[36vw] md:-mt-[18vw] rounded-[3.2vw] flex flex-col justify-center items-center bg-[#F1DE00] md:rounded-[1.56vw] relative z-[5] gap-2"
+        className="md:min-w-[29.2vw] md:h-[18.3vw] px-[20px] min-w-[60.3vw] h-[35.7vw] -mt-[36vw] md:-mt-[18vw] rounded-[3.2vw] flex flex-col justify-center items-center bg-[#F1DE00] md:rounded-[1.56vw] relative z-[15] gap-2"
       >
         
         {inViewport && data && (
@@ -72,7 +72,7 @@ const PageTwo = () => {
         <div className="md:h-[6px] h-[2px] w-full bg-[#FFEA00] absolute top-1/2 left-0 -mt-[3.9vw] md:-mt-[1.88vw] z-50"></div>
       </div>
 
-      <div className="md:min-w-[36.7vw] md:h-[18.3vw] md:ml-[5vw] min-w-[74.67vw] h-[35.7vw] px-[20px] mt-[6vw]  md:-mt-[18vw] flex flex-col justify-center items-center bg-[#221F00] md:rounded-[1.56vw] rounded-[3.2vw] relative z-[5]">
+      <div className="md:min-w-[36.7vw] md:h-[18.3vw] md:ml-[5vw] min-w-[74.67vw] h-[35.7vw] px-[20px] mt-[6vw]  md:-mt-[18vw] flex flex-col justify-center items-center bg-[#221F00] md:rounded-[1.56vw] rounded-[3.2vw] relative z-[15]">
       
         {inViewport && data && (
           <FilpClock
