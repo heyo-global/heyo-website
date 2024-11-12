1cd76950dc91c7e20c191be7b511abe805fd053e
chengmingyan <345021109@qq.com>
2024-11-12 20:09:05 +0800

    fix zindex

--- components/home/Phone.tsx
+++ components/home/Phone.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Phone = () => {
   return (
     <div className="md:w-[15.2vw] md:h-[28vw] w-[48.3vw] h-[89.1vw] overflow-hidden absolute left-1/2 -bottom-[36vw] -ml-[24.15vw] md:-bottom-[12vw] md:-ml-[7.6vw] z-[1500]">
-      <img src="/bg-phone.svg" className="w-full h-full img-disabled z-[1]" alt="" />
+      <img src="/bg-phone.svg" className="w-full h-full z-[1]" alt="" />
       <img
         src="/heyo-share-t.png"
         className="md:w-[12vw] md:h-[12vw] w-[36vw] h-[36vw] absolute left-1/2 -ml-[18vw] top-[10vw] md:-ml-[6vw] md:top-[3vw] z-10"
