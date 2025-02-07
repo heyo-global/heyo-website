c9701e812d53eb1ca1525e5ffc14e2a4581aaa63
chengmingyan <345021109@qq.com>
2025-02-07 11:24:43 +0800

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import '../components/home/spots.css';
-import '../components/home/cities.css';
+import "../components/home/spots.css";
+import "../components/home/cities.css";
 // import Header from "@/components/Header";
 import { Providers } from "@/providers/Providers";
 const inter = Inter({ subsets: ["latin"] });
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Heyo",
@@ -22,8 +23,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           {/* <Header /> */}
-          <div className="max-width-[1920px] mx-auto" id="root">{children}</div>
+          <div className="max-width-[1920px] mx-auto" id="root">
+            {children}
+          </div>
         </Providers>
+        <GoogleAnalytics gaId="GTM-NSPJXPWS" />
       </body>
     </html>
   );
